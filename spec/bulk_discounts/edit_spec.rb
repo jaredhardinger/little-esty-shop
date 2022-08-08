0943ebdcfe6ecbc8288492c55b93b2cf17d1147a
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-08-08 12:36:33 -0700

    had extra unneeded spec file/deleted

--- spec/bulk_discounts/edit_spec.rb
+++ spec/bulk_discounts/edit_spec.rb
@@ -2,18 +2,6 @@ require 'rails_helper'
 
 RSpec.describe "Bulk Discount Edit Page", type: :feature do 
 
-# Merchant Bulk Discount Edit
-
-# As a merchant
-# When I visit my bulk discount show page
-# Then I see a link to edit the bulk discount
-# When I click this link
-# Then I am taken to a new page with a form to edit the discount
-# And I see that the discounts current attributes are pre-poluated in the form
-# When I change any/all of the information and click submit
-# Then I am redirected to the bulk discount's show page
-# And I see that the discount's attributes have been updated
-
   it "has a link on the bulk discount show page to edit the bulk discount" do 
     merchant1 = Merchant.create!(name: "Poke Retirement homes")
     discount1 = BulkDiscount.create!(percentage: 20, quantity_threshold: 10, merchant_id: merchant1.id)
