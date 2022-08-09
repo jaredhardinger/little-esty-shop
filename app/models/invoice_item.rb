d4eebba2909d225117e18c033420d7729db3fbdf
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-08-09 10:23:25 -0700

    cleaned up admin show_spec.rb

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -20,7 +20,4 @@ class InvoiceItem < ApplicationRecord
     end
 end
 
-# maybe this will work
-# def get_max_discount_2
-#     
-#   end
+
--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -1,150 +1,157 @@
 require 'rails_helper'
 
 RSpec.describe 'Admin Invoices Item Index' do
-    describe 'As a Invoice' do
-        describe 'When I visit the admin Invoices Show page' do
+  describe 'As an Invoice' do
+    describe 'When I visit the admin Invoices Show page' do
 
-            it "is able to list all of the invoices atrributes " do
+      it "is able to list all of the invoices atrributes " do
 
-              merchant1 = Merchant.create!(name: "Poke Retirement homes")
-        			merchant2 = Merchant.create!(name: "Rendolyn Guizs poke stops")
-        			merchant3 = Merchant.create!(name: "Dhirley Secasrio's knits and bits")
+        merchant1 = Merchant.create!(name: "Poke Retirement homes")
+        merchant2 = Merchant.create!(name: "Rendolyn Guizs poke stops")
+        merchant3 = Merchant.create!(name: "Dhirley Secasrio's knits and bits")
 
-        			item1 = Item.create!(name: "Pikachu pics", description: 'Cute pics with pikachu', unit_price: 1000, merchant_id: merchant1.id)
-              item2 = Item.create!(name: "Pokemon stuffy", description: 'Pikachu stuffed toy', unit_price: 2000, merchant_id: merchant1.id)
-              item3 = Item.create!(name: "Junk", description: 'junk you should want', unit_price: 500, merchant_id: merchant2.id)
+        item1 = Item.create!(name: "Pikachu pics", description: 'Cute pics with pikachu', unit_price: 1000, merchant_id: merchant1.id)
+        item2 = Item.create!(name: "Pokemon stuffy", description: 'Pikachu stuffed toy', unit_price: 2000, merchant_id: merchant1.id)
+        item3 = Item.create!(name: "Junk", description: 'junk you should want', unit_price: 500, merchant_id: merchant2.id)
 
-        			customer1 = Customer.create!(first_name: "Parker", last_name: "Thomson")
-        			invoice1 = Invoice.create!(status: "completed", customer_id: customer1.id)
-              invoice2 = Invoice.create!(status: "completed", customer_id: customer1.id)
-        			transaction1 = Transaction.create!(credit_card_number: "123456789123456789", result: "success", invoice_id: invoice1.id)
-        		  invoice_item1 = InvoiceItem.create!(quantity: 1, unit_price: item1.unit_price, status: "shipped", item_id: item1.id, invoice_id: invoice1.id)
-              invoice_item2 = InvoiceItem.create!(quantity: 1, unit_price: item1.unit_price, status: "shipped", item_id: item2.id, invoice_id: invoice1.id)
-              invoice_item3 = InvoiceItem.create!(quantity: 1, unit_price: item1.unit_price, status: "shipped", item_id: item3.id, invoice_id: invoice2.id)
+        customer1 = Customer.create!(first_name: "Parker", last_name: "Thomson")
+        invoice1 = Invoice.create!(status: "completed", customer_id: customer1.id)
+        invoice2 = Invoice.create!(status: "completed", customer_id: customer1.id)
+        transaction1 = Transaction.create!(credit_card_number: "123456789123456789", result: "success", invoice_id: invoice1.id)
+        invoice_item1 = InvoiceItem.create!(quantity: 1, unit_price: item1.unit_price, status: "shipped", item_id: item1.id, invoice_id: invoice1.id)
+        invoice_item2 = InvoiceItem.create!(quantity: 1, unit_price: item1.unit_price, status: "shipped", item_id: item2.id, invoice_id: invoice1.id)
+        invoice_item3 = InvoiceItem.create!(quantity: 1, unit_price: item1.unit_price, status: "shipped", item_id: item3.id, invoice_id: invoice2.id)
 
-              visit "/admin/invoices/#{invoice1.id}"
+        visit "/admin/invoices/#{invoice1.id}"
 
-              expect(page).to have_content("Pikachu pics")
-              expect(page).to have_content(1)
-              expect(page).to have_content(1000)
-              expect(page).to have_content("shipped")
+        expect(page).to have_content("Pikachu pics")
+        expect(page).to have_content(1)
+        expect(page).to have_content(1000)
+        expect(page).to have_content("shipped")
 
-            end
+      end
 
-            it "is able to list all of the invoices atrributes " do
+      it "is able to list all of the invoices atrributes " do
 
-              merchant1 = Merchant.create!(name: "Poke Retirement homes")
-        			merchant2 = Merchant.create!(name: "Rendolyn Guizs poke stops")
-        			merchant3 = Merchant.create!(name: "Dhirley Secasrio's knits and bits")
+        merchant1 = Merchant.create!(name: "Poke Retirement homes")
+        merchant2 = Merchant.create!(name: "Rendolyn Guizs poke stops")
+        merchant3 = Merchant.create!(name: "Dhirley Secasrio's knits and bits")
 
-        			item1 = Item.create!(name: "Pikachu pics", description: 'Cute pics with pikachu', unit_price: 1000, merchant_id: merchant1.id)
-              item2 = Item.create!(name: "Pokemon stuffy", description: 'Pikachu stuffed toy', unit_price: 2000, merchant_id: merchant1.id)
-              item3 = Item.create!(name: "Junk", description: 'junk you should want', unit_price: 500, merchant_id: merchant2.id)
+        item1 = Item.create!(name: "Pikachu pics", description: 'Cute pics with pikachu', unit_price: 1000, merchant_id: merchant1.id)
+        item2 = Item.create!(name: "Pokemon stuffy", description: 'Pikachu stuffed toy', unit_price: 2000, merchant_id: merchant1.id)
+        item3 = Item.create!(name: "Junk", description: 'junk you should want', unit_price: 500, merchant_id: merchant2.id)
 
-        			customer1 = Customer.create!(first_name: "Parker", last_name: "Thomson")
-        			invoice1 = Invoice.create!(status: "completed", customer_id: customer1.id)
-              invoice2 = Invoice.create!(status: "completed", customer_id: customer1.id)
-        			transaction1 = Transaction.create!(credit_card_number: "123456789123456789", result: "success", invoice_id: invoice1.id)
-        		  invoice_item1 = InvoiceItem.create!(quantity: 1, unit_price: item1.unit_price, status: "shipped", item_id: item1.id, invoice_id: invoice1.id)
-              invoice_item2 = InvoiceItem.create!(quantity: 1, unit_price: item1.unit_price, status: "shipped", item_id: item2.id, invoice_id: invoice1.id)
-              invoice_item3 = InvoiceItem.create!(quantity: 1, unit_price: item1.unit_price, status: "shipped", item_id: item3.id, invoice_id: invoice2.id)
+        customer1 = Customer.create!(first_name: "Parker", last_name: "Thomson")
+        invoice1 = Invoice.create!(status: "completed", customer_id: customer1.id)
+        invoice2 = Invoice.create!(status: "completed", customer_id: customer1.id)
+        transaction1 = Transaction.create!(credit_card_number: "123456789123456789", result: "success", invoice_id: invoice1.id)
+        invoice_item1 = InvoiceItem.create!(quantity: 1, unit_price: item1.unit_price, status: "shipped", item_id: item1.id, invoice_id: invoice1.id)
+        invoice_item2 = InvoiceItem.create!(quantity: 1, unit_price: item1.unit_price, status: "shipped", item_id: item2.id, invoice_id: invoice1.id)
+        invoice_item3 = InvoiceItem.create!(quantity: 1, unit_price: item1.unit_price, status: "shipped", item_id: item3.id, invoice_id: invoice2.id)
 
-              visit "/admin/invoices/#{invoice2.id}"
+        visit "/admin/invoices/#{invoice2.id}"
 
-              expect(page).to have_content("Junk")
-              expect(page).to have_content(1)
-              expect(page).to have_content(1000)
-              expect(page).to have_content("shipped")
+        expect(page).to have_content("Junk")
+        expect(page).to have_content(1)
+        expect(page).to have_content(1000)
+        expect(page).to have_content("shipped")
 
-            end
+      end
 
-            it "is able to list all of the invoices total Revenue " do
+      it "is able to list all of the invoices total Revenue " do
 
-              merchant1 = Merchant.create!(name: "Poke Retirement homes")
-        			merchant2 = Merchant.create!(name: "Rendolyn Guizs poke stops")
-        			merchant3 = Merchant.create!(name: "Dhirley Secasrio's knits and bits")
+        merchant1 = Merchant.create!(name: "Poke Retirement homes")
+        merchant2 = Merchant.create!(name: "Rendolyn Guizs poke stops")
+        merchant3 = Merchant.create!(name: "Dhirley Secasrio's knits and bits")
 
-        			item1 = Item.create!(name: "Pikachu pics", description: 'Cute pics with pikachu', unit_price: 1000, merchant_id: merchant1.id)
-              item2 = Item.create!(name: "Pokemon stuffy", description: 'Pikachu stuffed toy', unit_price: 2000, merchant_id: merchant1.id)
-              item3 = Item.create!(name: "Junk", description: 'junk you should want', unit_price: 500, merchant_id: merchant2.id)
+        item1 = Item.create!(name: "Pikachu pics", description: 'Cute pics with pikachu', unit_price: 1000, merchant_id: merchant1.id)
+        item2 = Item.create!(name: "Pokemon stuffy", description: 'Pikachu stuffed toy', unit_price: 2000, merchant_id: merchant1.id)
+        item3 = Item.create!(name: "Junk", description: 'junk you should want', unit_price: 500, merchant_id: merchant2.id)
 
-        			customer1 = Customer.create!(first_name: "Parker", last_name: "Thomson")
-        			invoice1 = Invoice.create!(status: "completed", customer_id: customer1.id)
-              invoice2 = Invoice.create!(status: "completed", customer_id: customer1.id)
-        			transaction1 = Transaction.create!(credit_card_number: "123456789123456789", result: "success", invoice_id: invoice1.id)
-        		  invoice_item1 = InvoiceItem.create!(quantity: 1, unit_price: item1.unit_price, status: "shipped", item_id: item1.id, invoice_id: invoice1.id)
-              invoice_item2 = InvoiceItem.create!(quantity: 1, unit_price: item1.unit_price, status: "shipped", item_id: item2.id, invoice_id: invoice1.id)
-              invoice_item3 = InvoiceItem.create!(quantity: 1, unit_price: item1.unit_price, status: "shipped", item_id: item3.id, invoice_id: invoice2.id)
+        customer1 = Customer.create!(first_name: "Parker", last_name: "Thomson")
+        invoice1 = Invoice.create!(status: "completed", customer_id: customer1.id)
+        invoice2 = Invoice.create!(status: "completed", customer_id: customer1.id)
+        transaction1 = Transaction.create!(credit_card_number: "123456789123456789", result: "success", invoice_id: invoice1.id)
+        invoice_item1 = InvoiceItem.create!(quantity: 1, unit_price: item1.unit_price, status: "shipped", item_id: item1.id, invoice_id: invoice1.id)
+        invoice_item2 = InvoiceItem.create!(quantity: 1, unit_price: item1.unit_price, status: "shipped", item_id: item2.id, invoice_id: invoice1.id)
+        invoice_item3 = InvoiceItem.create!(quantity: 1, unit_price: item1.unit_price, status: "shipped", item_id: item3.id, invoice_id: invoice2.id)
 
-              visit "/admin/invoices/#{invoice2.id}"
-              expect(page).to have_content("Junk")
-              expect(page).to have_content(1)
-              expect(page).to have_content(1000)
-              expect(page).to have_content("shipped")
-              expect(page).to have_content("Total revenue: 1000")
-              expect(page).to_not have_content("Total revenue: 2000")
+        visit "/admin/invoices/#{invoice2.id}"
+        expect(page).to have_content("Junk")
+        expect(page).to have_content(1)
+        expect(page).to have_content(1000)
+        expect(page).to have_content("shipped")
+        expect(page).to have_content("Total revenue: 1000")
+        expect(page).to_not have_content("Total revenue: 2000")
 
-            end
+      end
 
-            it "is able to list all of the invoices total revenue " do
+      it "is able to list all of the invoices total revenue " do
 
-              merchant1 = Merchant.create!(name: "Poke Retirement homes")
-        			merchant2 = Merchant.create!(name: "Rendolyn Guizs poke stops")
-        			merchant3 = Merchant.create!(name: "Dhirley Secasrio's knits and bits")
+        merchant1 = Merchant.create!(name: "Poke Retirement homes")
+        merchant2 = Merchant.create!(name: "Rendolyn Guizs poke stops")
+        merchant3 = Merchant.create!(name: "Dhirley Secasrio's knits and bits")
 
-        			item1 = Item.create!(name: "Pikachu pics", description: 'Cute pics with pikachu', unit_price: 1000, merchant_id: merchant1.id)
-              item2 = Item.create!(name: "Pokemon stuffy", description: 'Pikachu stuffed toy', unit_price: 2000, merchant_id: merchant1.id)
-              item3 = Item.create!(name: "Junk", description: 'junk you should want', unit_price: 500, merchant_id: merchant2.id)
+        item1 = Item.create!(name: "Pikachu pics", description: 'Cute pics with pikachu', unit_price: 1000, merchant_id: merchant1.id)
+        item2 = Item.create!(name: "Pokemon stuffy", description: 'Pikachu stuffed toy', unit_price: 2000, merchant_id: merchant1.id)
+        item3 = Item.create!(name: "Junk", description: 'junk you should want', unit_price: 500, merchant_id: merchant2.id)
 
-        			customer1 = Customer.create!(first_name: "Parker", last_name: "Thomson")
-        			invoice1 = Invoice.create!(status: "completed", customer_id: customer1.id)
-              invoice2 = Invoice.create!(status: "completed", customer_id: customer1.id)
-        			transaction1 = Transaction.create!(credit_card_number: "123456789123456789", result: "success", invoice_id: invoice1.id)
-        		  invoice_item1 = InvoiceItem.create!(quantity: 1, unit_price: item1.unit_price, status: "shipped", item_id: item1.id, invoice_id: invoice1.id)
-              invoice_item2 = InvoiceItem.create!(quantity: 1, unit_price: item1.unit_price, status: "shipped", item_id: item2.id, invoice_id: invoice1.id)
-              invoice_item3 = InvoiceItem.create!(quantity: 1, unit_price: item1.unit_price, status: "shipped", item_id: item3.id, invoice_id: invoice2.id)
+        customer1 = Customer.create!(first_name: "Parker", last_name: "Thomson")
+        invoice1 = Invoice.create!(status: "completed", customer_id: customer1.id)
+        invoice2 = Invoice.create!(status: "completed", customer_id: customer1.id)
+        transaction1 = Transaction.create!(credit_card_number: "123456789123456789", result: "success", invoice_id: invoice1.id)
+        invoice_item1 = InvoiceItem.create!(quantity: 1, unit_price: item1.unit_price, status: "shipped", item_id: item1.id, invoice_id: invoice1.id)
+        invoice_item2 = InvoiceItem.create!(quantity: 1, unit_price: item1.unit_price, status: "shipped", item_id: item2.id, invoice_id: invoice1.id)
+        invoice_item3 = InvoiceItem.create!(quantity: 1, unit_price: item1.unit_price, status: "shipped", item_id: item3.id, invoice_id: invoice2.id)
 
-              visit "/admin/invoices/#{invoice1.id}"
+        visit "/admin/invoices/#{invoice1.id}"
 
-              expect(page).to have_content("Pikachu pics")
-              expect(page).to have_content(1)
-              expect(page).to have_content(1000)
-              expect(page).to have_content("shipped")
-              expect(page).to have_content("Total revenue: 2000")
-              expect(page).to_not have_content("Total revenue: 1000")
-            end
+        expect(page).to have_content("Pikachu pics")
+        expect(page).to have_content(1)
+        expect(page).to have_content(1000)
+        expect(page).to have_content("shipped")
+        expect(page).to have_content("Total revenue: 2000")
+        expect(page).to_not have_content("Total revenue: 1000")
+      end
 
 
-            it "is able to list all of the invoices atrributes " do
+      it "is able to list all of the invoices atrributes " do
 
-              merchant1 = Merchant.create!(name: "Poke Retirement homes")
-              merchant2 = Merchant.create!(name: "Rendolyn Guizs poke stops")
-              merchant3 = Merchant.create!(name: "Dhirley Secasrio's knits and bits")
+        merchant1 = Merchant.create!(name: "Poke Retirement homes")
+        merchant2 = Merchant.create!(name: "Rendolyn Guizs poke stops")
+        merchant3 = Merchant.create!(name: "Dhirley Secasrio's knits and bits")
 
-              item1 = Item.create!(name: "Pikachu pics", description: 'Cute pics with pikachu', unit_price: 1000, merchant_id: merchant1.id)
-              item2 = Item.create!(name: "Pokemon stuffy", description: 'Pikachu stuffed toy', unit_price: 2000, merchant_id: merchant1.id)
-              item3 = Item.create!(name: "Junk", description: 'junk you should want', unit_price: 500, merchant_id: merchant2.id)
+        item1 = Item.create!(name: "Pikachu pics", description: 'Cute pics with pikachu', unit_price: 1000, merchant_id: merchant1.id)
+        item2 = Item.create!(name: "Pokemon stuffy", description: 'Pikachu stuffed toy', unit_price: 2000, merchant_id: merchant1.id)
+        item3 = Item.create!(name: "Junk", description: 'junk you should want', unit_price: 500, merchant_id: merchant2.id)
 
-              customer1 = Customer.create!(first_name: "Parker", last_name: "Thomson")
-              invoice1 = Invoice.create!(status: "completed", customer_id: customer1.id)
-              invoice2 = Invoice.create!(status: "completed", customer_id: customer1.id)
-              transaction1 = Transaction.create!(credit_card_number: "123456789123456789", result: "success", invoice_id: invoice1.id)
-              invoice_item1 = InvoiceItem.create!(quantity: 1, unit_price: item1.unit_price, status: "shipped", item_id: item1.id, invoice_id: invoice1.id)
-              invoice_item2 = InvoiceItem.create!(quantity: 1, unit_price: item1.unit_price, status: "shipped", item_id: item2.id, invoice_id: invoice1.id)
-              invoice_item3 = InvoiceItem.create!(quantity: 1, unit_price: item1.unit_price, status: "shipped", item_id: item3.id, invoice_id: invoice2.id)
+        customer1 = Customer.create!(first_name: "Parker", last_name: "Thomson")
+        invoice1 = Invoice.create!(status: "completed", customer_id: customer1.id)
+        invoice2 = Invoice.create!(status: "completed", customer_id: customer1.id)
+        transaction1 = Transaction.create!(credit_card_number: "123456789123456789", result: "success", invoice_id: invoice1.id)
+        invoice_item1 = InvoiceItem.create!(quantity: 1, unit_price: item1.unit_price, status: "shipped", item_id: item1.id, invoice_id: invoice1.id)
+        invoice_item2 = InvoiceItem.create!(quantity: 1, unit_price: item1.unit_price, status: "shipped", item_id: item2.id, invoice_id: invoice1.id)
+        invoice_item3 = InvoiceItem.create!(quantity: 1, unit_price: item1.unit_price, status: "shipped", item_id: item3.id, invoice_id: invoice2.id)
 
-              visit "/admin/invoices/#{invoice1.id}"
+        visit "/admin/invoices/#{invoice1.id}"
 
-              expect(page).to have_content('completed')
+        expect(page).to have_content('completed')
 
-              select "in progress", :from => "Status"
-              click_on 'Update Invoice'
+        select "in progress", :from => "Status"
+        click_on 'Update Invoice'
 
-              expect(page).to have_content('completed')
-            end
+        expect(page).to have_content('completed')
+      end
 
-        end
+# Admin Invoice Show Page: Total Revenue and Discounted Revenue
 
+# As an admin
+# When I visit an admin invoice show page
+# Then I see the total revenue from this invoice (not including discounts)
+# And I see the total discounted revenue from this invoice which includes bulk discounts in the calculation
+      it "can list the total revenue with and without bulk discounts for an invoice" do 
+        
+      end
     end
-
+  end
 end
